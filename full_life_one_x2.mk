f469ea515688c6fe25b2a034a66fc4bd63bba071
Your Name <you@example.com>
2018-07-24 05:51:37 -0800

    Added full BLOX2 makefile and device make file.

--- full_life_one_x2.mk
+++ full_life_one_x2.mk
@@ -15,20 +15,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-$(call inherit-product, device/blu/life_one_x2/full_life_one_x2.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from BLOX2 device
+$(call inherit-product, device/blu/life_one_x2/device.mk)
 
-PRODUCT_NAME := lineage_life_one_x2
-BOARD_VENDOR := blu
-
-# TODO: What is this?
-#PRODUCT_GMS_CLIENTID_BASE := android-micromax
-
-## Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=BLU/Life_One_X2/Life_One_X2:6.0.1/MMB29M/1477622278:user/release-keys \
-    PRIVATE_BUILD_DESC="p7701-user 6.0.1 MMB29M eng.android.1477619128 release-keys"
-endif
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := life_one_x2
+PRODUCT_NAME := full_life_one_x2
+PRODUCT_BRAND := BLU
+PRODUCT_MODEL := life_one_x2
+PRODUCT_MANUFACTURER := BLU
