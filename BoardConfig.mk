b94790a9a0579af47f98bc2e9257669d7e44357f
Your Name <you@example.com>
2018-07-27 11:43:25 -0800

    license

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
+# Edited by AlaskaLinuxUser
 
 LOCAL_PATH := device/blu/life_one_x2
 
