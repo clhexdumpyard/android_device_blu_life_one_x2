4d1cf727ff1f55f1eff09eee2da95578b0726f1a
alaskalinuxuser <alaskalinuxuser@gmail.com>
2018-09-21 06:17:46 -0800

    updates.

--- device.mk
+++ device.mk
@@ -117,9 +117,9 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
 # Camera
-PRODUCT_PACKAGES += \
-    camera.msm8937 \
-    Snap
+#PRODUCT_PACKAGES += \
+    #camera.msm8937 \
+    #Snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/msm8937_camera_p7201.xml:system/etc/camera/msm8937_camera_p7201.xml \
